fd74e72987628e3959d725dd209db784e38a3929
wendytamara <anacarlavegam@gmail.com>
2020-01-31 11:40:36 -0500

    Auto-generated commit

--- src/app/pipes/img-tecnologia.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImgTecnologiaPipe } from './img-tecnologia.pipe';
-
-describe('ImgTecnologiaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ImgTecnologiaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
